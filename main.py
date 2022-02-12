2e9b0fa68482a5d4e260ee68fe52549408472fa4
Achilleas Tzimis <AchilleasTzimis@Jaydors-MacBook-Pro.local>
2022-02-12 17:20:16 +0000

    first

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(num):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('second')
+    print_hi('third')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
