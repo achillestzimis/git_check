3aef5c1a687f089cea14fec32d66b11eb47c38b9
Achilleas Tzimis <AchilleasTzimis@Jaydors-MacBook-Pro.local>
2022-02-12 17:21:26 +0000

    fifth

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(num):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('fourth')
+    print_hi('fifth')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
